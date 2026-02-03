83f46b849ba13cd317609073655d76c025308235
Sacha Cazin--Lepczynski <cazin.lep@gmail.com>
2026-02-03 22:08:17 +0100

    dnspython v1
    First test on dnspython on google.com

---
+++ dnspython.py
@@ -0,0 +1,39 @@
+import dns.resolver
+import dns.exception
+import json
+
+def connection_test(server_ip, server_name) :
+    
+    tester = dns.resolver.Resolver()
+
+    tester.nameservers = [server_ip]
+
+    tester.lifetime = 2.0
+
+    res = {
+        "ip :": server_ip,
+        "name": server_name,
+        "service": "DNS",
+        "statue": "OK",
+        "response_details": ""
+    }
+
+    try : 
+        response = tester.resolve('google.com', 'A')
+        res["response_details"] = f"Test OK: {response[0]}"
+        return res
+    
+    except dns.resolver.Timeout:    
+        res["statue"] = "ERREUR"
+        res["response_details"] = "Timeout : Le service DNS ne repond pas."
+
+    except dns.resolver.NoNameservers:
+        res["statue"] = "ERREUR"
+        res["details"] = "Impossible de contacter le domain name server#."
+
+    return res
+
+
+#test = connection_test("192.168.152.245", "server_name")
+
+#print(json.dumps(test, indent=2))
