fa7d27a4121551743f8a93bf85c8dc54ca9e36c9
CazinSacha <cazin.lep@gmail.com>
2026-02-11 20:34:44 +0100

    First Test AdPython

--- adpython.py
+++ adpython.py
@@ -1,1 +1,19 @@
-from ldap3 import Server, Connection
+from ldap3 import Server, Connection, ALL, NTLM, Tls
+import ssl
+
+tls_config = Tls(validate=ssl.CERT_NONE, version=ssl.PROTOCOL_TLSv1_2)
+
+server = Server('192.168.139.142',
+                port=636,
+                use_ssl=True, 
+                get_info=ALL,
+                tls=tls_config)
+
+connection = Connection(server, 
+                        user='TESTSRV\\Administrateur',
+                        password='Formation2025',
+                        authentication=NTLM,
+                        auto_bind=True)
+
+print(f"Connecté : {connection.bound}")
+
